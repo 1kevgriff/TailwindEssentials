615c6c7b147653e9988eab9ee45657e116c2b0c4
Kevin Griffin <kevin@kevgriffin.com>
2022-03-26 10:59:25 -0400

    wip

---
+++ Code/StarterDemo/END/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <div class="bg-black text-white w-1/2 m-10 p-10 mx-auto">
+    <div
+      class="mx-auto border bg-contain bg-center bg-no-repeat w-64 h-64"
+      style="background-image: url(/src/assets/motivational.gif"
+    ></div>
+    <p class="text-center text-3xl pt-5 font-mono uppercase">I hate CSS</p>
+  </div>
+</template>
+
+<style>
+</style>
