ec52617752999c0c0cbe3125e03ee39510ad2ba5
Kevin Griffin <kevin@kevgriffin.com>
2022-03-30 19:53:46 -0400

    responsive design

---
+++ Code/ResponsiveFlexBox/START/src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Super Awesome App</h1>
+    </header>
+    <main>
+      <div>
+        <div>
+          <img src="http://placekitten.com/100/100" />
+          <p>Good cat</p>
+        </div>
+        <div>
+          <img src="http://placekitten.com/100/100" />
+          <p>Okay cat</p>
+        </div>
+        <div>
+          <img src="http://placekitten.com/100/100" />
+          <p>Bad cat</p>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nisi cumque
+        explicabo quam. Aliquam unde, magni ullam aperiam cumque at odit dolorem
+        quis recusandae. Deserunt fuga ad mollitia aliquam. Consectetur.
+      </p>
+    </main>
+
+    <footer>
+      <p>© Kevin Griffin</p>
+    </footer>
+  </div>
+</template>
+
+<style>
+</style>
