75ca10772d6681f81546ef9f40910b5ecbb22573
Kevin Griffin <kevin@kevgriffin.com>
2022-03-29 22:30:55 -0400

    wip

---
+++ Code/ButtonDemoApply/END/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="m-10">
+    <button class="btn">👆 Click Me</button>
+    <button class="btn">👆 Click Me</button>
+    <button class="btn">👆 Click Me</button>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ Code/ButtonDemoApply/START/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="m-10">
+    <button
+      class="p-2 bg-pink-300 border-2 border-blue-700 rounded-md shadow-lg hover:bg-pink-200 shadow-neutral-500 hover:animate-bounce"
+    >
+      👆 Click Me
+    </button>
+  </div>
+</template>
+
+<style>
+</style>
